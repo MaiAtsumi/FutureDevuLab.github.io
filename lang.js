5c1bc49998cff39fa90ef32d0818846683e8226d
Cosimo Riondino <cosimo.riondino@horizon-team.com>
2019-01-02 23:52:23 +0100

    0.1.1 - Multilanguage plugin installed
    Some languages yay!

---
+++ lang.js
@@ -0,0 +1,25 @@
+var langs = ['en', 'it'];
+var langCode = '';
+var langJS = null;
+
+
+var translate = function (jsdata)
+{	
+	$("[tkey]").each (function (index)
+	{
+		var strTr = jsdata [$(this).attr ('tkey')];
+	    $(this).html (strTr);
+	});
+}
+
+
+langCode = navigator.language.substr (0, 2);
+alert(langCode);
+
+if (langs.includes(langCode))
+	$.getJSON('lang/'+langCode+'.json', translate);
+else
+	$.getJSON('lang/en.json', translate);
+
+
+
